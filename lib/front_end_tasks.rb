1b715cd5c7562f1aa5985565be124f758a047dd4
Mike Enriquez <mike@enriquez.me>
2014-03-29 03:00:36 -0400

    Move logic for better scriptability

--- lib/front_end_tasks.rb
+++ lib/front_end_tasks.rb
@@ -1,4 +1,42 @@
 require "front_end_tasks/version"
 
+require 'front_end_tasks/optimizer'
+require 'front_end_tasks/server'
+require 'front_end_tasks/lint'
+require 'front_end_tasks/spec'
+require 'front_end_tasks/documents'
+
 module FrontEndTasks
+
+  def self.build(public_dir, build_dir, *files)
+    optimizer = Optimizer.new(public_dir, files)
+    optimizer.build_to(build_dir)
+  end
+
+  def self.server(options)
+    Server.start(options)
+  end
+
+  def self.lint(*files)
+    Lint.report(files)
+  end
+
+  def self.spec(options)
+    Spec.run(options)
+  end
+
+  def self.list_scripts(file, public_root = nil)
+    content = File.read(File.expand_path(file))
+    extension = File.extname(file).downcase
+    doc = nil
+
+    if extension == '.html'
+      doc = Documents::HtmlDocument.new(nil, content)
+    elsif extension == '.js'
+      doc = Documents::JsDocument.new(nil, content)
+    end
+
+    doc.included_scripts(public_root)
+  end
+
 end
--- lib/front_end_tasks/cli.rb
+++ lib/front_end_tasks/cli.rb
@@ -1,8 +1,5 @@
 require 'thor'
-require 'front_end_tasks/optimizer'
-require 'front_end_tasks/server'
-require 'front_end_tasks/lint'
-require 'front_end_tasks/spec'
+require 'front_end_tasks'
 
 module FrontEndTasks
 
@@ -11,20 +8,19 @@ module FrontEndTasks
     desc "build", "Builds the given files according to special build comments"
     method_option :result, :default => File.expand_path('./build')
     def build(public_dir, *files)
-      optimizer = Optimizer.new(public_dir, files)
-      optimizer.build_to(options[:result])
+      FrontEndTasks.build(public_dir, options[:result], files)
     end
 
     desc "server", "Run a static site directory on localhost"
     method_option :public_dir, :default => File.expand_path('./build')
     method_option :port, :default => 8000
-    def server()
-      Server.start(options)
+    def server
+      FrontEndTasks.server(options)
     end
 
     desc "lint", "Run JSLint"
     def lint(*files)
-      Lint.report(files)
+      FrontEndTasks.lint(files)
     end
 
     desc "spec", "Run Jasmine specs"
@@ -32,24 +28,14 @@ module FrontEndTasks
     method_option :helper_files, :type => :array, :default => []
     method_option :spec_files, :type => :array
     method_option :port, :default => 8001
-    def spec()
-      Spec.run(options)
+    def spec
+      FrontEndTasks.spec(options)
     end
 
     desc "list_scripts", "List dependent javascript files"
     method_option :public_root
     def list_scripts(file)
-      content = File.read(File.expand_path(file))
-      extension = File.extname(file).downcase
-      doc = nil
-
-      if extension == '.html'
-        doc = HtmlDocument.new(nil, content)
-      elsif extension == '.js'
-        doc = JsDocument.new(nil, content)
-      end
-
-      puts doc.included_scripts(options[:public_root])
+      puts FrontEndTasks.list_scripts(file, options[:public_root])
     end
 
   end
